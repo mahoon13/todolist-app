6f312160034b857c279e2174a611074173fedd62
mahoon13 <tbhmahoon@gmail.com>
2022-05-19 15:43:47 +0430

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,127 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import css from "./style.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: JSON.parse(localStorage.getItem("todolist")) || [],
+    };
+    this.input = React.createRef();
+    this.handleAddTodo = this.addTodo.bind(this);
+    this.handleRemoveCompleteTodos = this.handleRemoveCompleteTodos.bind(this);
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  componentDidMount() {
+    window.addEventListener("completetodo", (e) => {
+      this.setState({ todos: JSON.parse(localStorage.getItem("todolist")) });
+    });
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem("todolist", JSON.stringify(this.state.todos));
+  }
+
+  handleRemoveCompleteTodos() {
+    let todos = this.state.todos;
+    let uncompleteTodos = todos.filter((todo) => !todo.complete);
+    this.setState({ todos: uncompleteTodos });
+  }
+
+  addTodo(todo) {
+    let todoName = this.input.current.value;
+    if (todoName === "") return;
+
+    this.input.current.value = null;
+
+    let uuid;
+    if (this.state.todos.length === 0) {
+      uuid = 0;
+    } else {
+      uuid = this.state.todos[this.state.todos.length - 1].id + 1;
+    }
+    this.setState({
+      todos: [
+        ...this.state.todos,
+        {
+          id: uuid,
+          name: todoName,
+          complete: false,
+        },
+      ],
+    });
+  }
+
+  render() {
+    let todosLeft = this.state.todos.filter((todo) => !todo.complete);
+
+    return (
+      <>
+        <TodoList todolist={this.state.todos} />
+        <input ref={this.input} type="text"></input>
+        <button onClick={this.handleAddTodo}>add TODO</button>
+        <button onClick={this.handleRemoveCompleteTodos}>
+          clear complete TODOs
+        </button>
+        <div>{todosLeft.length} TODO Left</div>
+      </>
+    );
+  }
+}
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return this.props.todolist.map((todo) => {
+      return <Todo key={todo.id} id={todo.id} todo={todo} />;
+    });
+  }
+}
+
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleCheckBox = this.handleCheckBox.bind(this);
+  }
+
+  handleCheckBox() {
+    let todos = JSON.parse(localStorage.getItem("todolist"));
+    todos[this.findTodoIndex(this.props.todo)].complete =
+      !todos[this.findTodoIndex(this.props.todo)].complete;
+
+    localStorage.setItem("todolist", JSON.stringify(todos));
+    window.dispatchEvent(new Event("completetodo"));
+  }
+
+  findTodoIndex(todo) {
+    let todos = JSON.parse(localStorage.getItem("todolist"));
+    let todoId = todo.id;
+    for (let i in todos) {
+      if (todos[i].id === todoId) {
+        return i;
+      }
+    }
+    return null;
+  }
+
+  render() {
+    return (
+      <div className="todo">
+        <label>
+          <input
+            type="checkbox"
+            defaultChecked={this.props.todo.complete}
+            onClick={this.handleCheckBox}
+          ></input>
+          {this.props.todo.name}
+        </label>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
